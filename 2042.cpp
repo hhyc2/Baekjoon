225d7f7cad413a7a8c0a4f9bec762cb857cfe0d0
hhyc2 <51672937+hhyc2@users.noreply.github.com>
2021-03-08 16:36:17 +0900

    Update 2042.cpp

--- 2042.cpp
+++ 2042.cpp
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<cmath>
 #pragma warning (disable:4996)
-long long int arr[1 << 21]; // 2ÀÇ 21½Â ¼±¾ğ shift
+long long int arr[1 << 21]; // 2ì˜ 21ìŠ¹ ì„ ì–¸ shift
 int N, M, K;
-long long int sum(int itr, int start, int end, int left, int right)
+long long int sum(int itr, int start, int end, int left, long long int right)
 {
 	int mid = (start + end) / 2;
-	if ( end<left || right<start )
+	if (end < left || right < start)
 		return 0;
 	if (left <= start && end <= right)
 		return arr[itr];
@@ -15,19 +15,20 @@ long long int sum(int itr, int start, int end, int left, int right)
 int main()
 {
 	scanf("%d %d %d", &N, &M, &K);
-	int size = pow(2, (int)log2(N) + 1);
+	int size = pow(2, (int)log2(N - 1) + 1);
 	for (int i = 0; i < N; i++)
 		scanf("%lld", &arr[size + i]);
 	for (int i = size - 1; i > 0; i--)
-		arr[i] = arr[i * 2] + arr[i * 2 + 1]; // Æ®¸® »ı¼º
+		arr[i] = arr[i * 2] + arr[i * 2 + 1]; // íŠ¸ë¦¬ ìƒì„±
 	for (int i = 0; i < M + K; i++)
 	{
-		int a, b, c;
-		scanf("%d %d %d", &a, &b, &c);
+		int a, b;
+		long long int c;
+		scanf("%d %d %lld", &a, &b, &c);
 		if (a == 1)
 		{
 			int itr = size + b - 1;
-			int diff = c - arr[itr];
+			long long int diff = c - arr[itr];
 			arr[itr] = c;
 			for (itr = itr / 2; itr > 0; itr = itr / 2)
 				arr[itr] = arr[itr] + diff;
